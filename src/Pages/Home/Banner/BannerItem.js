de1591351c26ee862bd6cd05e75e84252e9ff2a2
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-11-09 14:27:30 +0600

    added carosel in banner side

---
+++ src/Pages/Home/Banner/BannerItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const BannerItem = ({ slide }) => {
+    const { image, id, prev, next } = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+            <div className='carousel-img'>
+                <img src={image} alt="" className="w-full h-full" />
+            </div>
+            <div className="absolute flex justify-center mx-auto transform -translate-y-1/2 left-2/4 right-50% top-1/4">
+                <h1 className='text-6xl font-bold text-white'>
+                    Travel <br /> With Us <br /> to get a great experience
+                </h1>
+            </div>
+            <div className="absolute flex justify-center mx-auto transform -translate-y-1/2 w-2/5 left-2/4 right-50% top-2/4">
+                <p className='text-xl text-white mt-3'>TAKE ADVANTAGE OF THIS AMAZING EXCLUSIVE OFFER
+                    DON'T MISS THIS OPPORTUNITY FOR YOUR BUSINESS</p>
+            </div>
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href={`#slide${prev}`} className="btn btn-circle">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+            </div>
+        </div>
+    );
+};
+
+export default BannerItem;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,9 +2,33 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer</h2>
-        </div>
+        <footer className="footer p-20 bg-black text-white">
+            <div>
+                <h1 className='text-5xl'>Lets Tour</h1>
+                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+            </div>
+            <div>
+                <span className="footer-title">Services</span>
+                <a href="/" className="link link-hover">Holyday</a>
+                <a href="/" className="link link-hover">Transport</a>
+                <a href="/" className="link link-hover">Hotel</a>
+                <a href="/" className="link link-hover">Resturant</a>
+                <a href="/" className="link link-hover">Tourist guide</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a href="/" className="link link-hover">About us</a>
+                <a href="/" className="link link-hover">Contact</a>
+                <a href="/" className="link link-hover">Jobs</a>
+                <a href="/" className="link link-hover">Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a href="/" className="link link-hover">Terms of use</a>
+                <a href="/" className="link link-hover">Privacy policy</a>
+                <a href="/" className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
